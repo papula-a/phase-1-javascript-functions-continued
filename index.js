df97fba12211979bac54b1f3daf4f0eb9f21ff3a
papula-a <pa20381@auis.edu.krd>
2024-04-11 13:56:31 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+console.log(saturdayFun()); 
+console.log(saturdayFun("hike"));
+
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+console.log(mondayWork()); 
+console.log(mondayWork("work from home"));
+
+function wrapAdjective(symbol = "*") {
+    return function(adjective) {
+        return `You are ${symbol}${adjective}${symbol}!`;
+    }
+}
